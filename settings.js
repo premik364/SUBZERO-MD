b38d24adc5c6009618de2b8a63fa3805e4fceeeb
premik364 <bpremik35@gmail.com>
2025-07-31 17:32:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;nB8WAR6J#2j_CFyD_WvKBakfKetE4zUPbEJWXJmQoZwwGm-X1uHU", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
